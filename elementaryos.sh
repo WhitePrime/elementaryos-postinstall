ed26984f9541ce0c3c0115b5c9e5525af2b1f8b2
Bruno Raphael <brcmesquita@outlook.com.br>
2016-10-01 16:45:30 -0300

    Create elementaryos.sh

---
+++ elementaryos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## GUIA PÓS INSTALAÇÃO LINUX ELEMENTARY ##
+
+#01 - Manter tudo atualizado
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt updade -y
+    sudo apt dist-upgrade -y
+    
+#02 - Primeira limpeza
+    sudo apt autoremove -y
+    sudo apt install -f -y
+  
+#03 - Adicionando repostirórios para instalação
+    
